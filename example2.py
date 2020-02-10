ee4f5fe9f7ed45fad59d5a8682a521dd15d1707e
zappyzac <zhaozong.ang@gmail.com>
2020-02-10 13:39:26 +0800

    numpy scipy

--- example2.py
+++ example2.py
@@ -1,6 +1,15 @@
 #testing numpy & scipy
 import numpy as np
+from scipy import stats
 
 my_array = np.array([1,2,3])
 print (my_array)
 print (type(my_array))
+
+m = np.array( [ [1,2], [3,4], [5,6] ] )
+print (m.shape)
+print (np.mean(m))
+
+
+sample = [1, 1, 1, 2, 3, 4, 5]
+stats.mode(sample)
